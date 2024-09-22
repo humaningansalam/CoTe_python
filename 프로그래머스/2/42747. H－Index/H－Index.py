29694854a7f7b50bf88544f2ca4de4bee6f3e701
Sanghun Yun <89466243+humaningansalam@users.noreply.github.com>
2024-09-22 15:04:43 +0900

    [level 2] Title: H-Index, Time: 0.24 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -0,0 +1,16 @@
+def solution(citations):
+    
+    answer = 0
+    
+    citations.sort(reverse=True)
+    
+    for i in range(len(citations)):
+        if citations[i] >= i+1:
+            answer = i + 1
+        else:
+            break
+            
+    print(answer)
+            
+    
+    return answer
